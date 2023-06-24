ed85b87cef5e2ce6ebf06b9c6ef708773e197c8c
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-06-24 14:57:25 +0530

    doubts

--- scripts/script.js
+++ scripts/script.js
@@ -5,28 +5,42 @@ const compScoreEl = document.getElementById(`comp-score`);
 const btnRockEl = document.getElementById(`btn-rock`);
 const btnPaperEl = document.getElementById(`btn-paper`);
 const btnScissorsEl = document.getElementById(`btn-scissors`);
-const ansEl = document.getElementById(`ans`);
+const userChoiceEl = document.getElementById(`user-choice`);
+const compChoiceEl = document.getElementById(`computer-choice`);
 const resultEl = document.getElementById(`result`);
 
 // global variables
 let userScore;
 let compScore;
+let result;
 // functions
 const getCompChoice = () => {
   weapons = [`rock`, `paper`, `scissors`];
   const randomNumber = Math.trunc(Math.random() * weapons.length);
   return weapons[randomNumber];
 };
+
+const getResult = function (userSelect) {
+  const computerSelect = getCompChoice();
+  if (userSelect === computerSelect) {
+    result = `game draw`;
+  } else if (
+    (userSelect === `rock` && computerSelect === `scissors`) ||
+    (userSelect === `paper` && computerSelect === `rock`) ||
+    (userSelect === `scissors` && computerSelect === `paper`)
+  ) {
+    result = `game won`;
+  } else {
+    result = `game lost`;
+  }
+};
 // event listeners
 btnRockEl.addEventListener(`click`, () => {
-  const computerChoice = getCompChoice();
-  if (btnRockEl === computerChoice) {
-    return `game draw`;
-  } else if (btnRockEl && computerChoice === `scissors`) {
-    return `game won`;
-  } else {
-    return `game lost`;
-  }
+  // console.log(getResult(rock));
+  const output = getResult(rock);
+  console.log(output);
+  console.log(`user selected: ${userSelect}`);
+  console.log(`computer selected: ${computerSelect}`);
 });
 
 // init settings
